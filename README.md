ada84ef3dd940671c37f40bca7c7d9e792b42208
joaovillarb <36734606+joaovillarb@users.noreply.github.com>
2021-05-09 14:55:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Client Tic Tac Toe  - Client Jogo da Velha
+Jogo destinado a cadeira de Redes 2 da UNICAP
---
+++ src/client/ClientTest.java
@@ -0,0 +1,18 @@
+package client;
+
+import javax.swing.JFrame;
+
+class ClientTest {
+    public static void main(String[] args) {
+        Client application; // declare client application
+
+        // Check for command line args
+        if (args.length == 0) {
+            application = new Client("127.0.0.1"); // localhost
+        } else {
+            application = new Client(args[0]); // use args
+        }
+
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
